67df24b0152d527591d57ff91dd47de3a643df4c
HuynhDuc <huynhduc96@gmail.com>
2020-05-18 17:18:19 +0700

    lesson8 - done 8.1

--- Lesson8-Internet/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ Lesson8-Internet/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -20,30 +20,60 @@ package com.example.android.marsrealestate.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.android.marsrealestate.network.MarsApi
+import com.example.android.marsrealestate.network.MarsProperty
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
  */
 class OverviewViewModel : ViewModel() {
 
-    // The internal MutableLiveData String that stores the status of the most recent request
-    private val _response = MutableLiveData<String>()
+  // The internal MutableLiveData String that stores the status of the most recent request
+  private val _response = MutableLiveData<String>()
+  private var viewModelJob = Job()
+  private val coroutineScope = CoroutineScope(
+      viewModelJob + Dispatchers.Main )
 
-    // The external immutable LiveData for the request status String
-    val response: LiveData<String>
-        get() = _response
+  // The external immutable LiveData for the request status String
+  val response: LiveData<String>
+    get() = _response
 
-    /**
-     * Call getMarsRealEstateProperties() on init so we can display status immediately.
-     */
-    init {
-        getMarsRealEstateProperties()
+  /**
+   * Call getMarsRealEstateProperties() on init so we can display status immediately.
+   */
+  init {
+    getMarsRealEstateProperties()
+  }
+
+  /**
+   * Sets the value of the status LiveData to the Mars API status.
+   */
+  private fun getMarsRealEstateProperties() {
+    coroutineScope.launch {
+      val getPropertiesDeferred =
+          MarsApi.retrofitService.getProperties()
+      try {
+        var listResult = getPropertiesDeferred.await()
+        _response.value =
+            "Success: ${listResult.size} Mars properties retrieved"
+      } catch (e: Exception) {
+        _response.value = "Failure: ${e.message}"
+      }
     }
+  }
 
-    /**
-     * Sets the value of the status LiveData to the Mars API status.
-     */
-    private fun getMarsRealEstateProperties() {
-        _response.value = "Set the Mars API Response here!"
-    }
+  override fun onCleared() {
+    super.onCleared()
+    viewModelJob.cancel()
+  }
+
 }
+
