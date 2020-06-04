27063d6aa6f45226691662d08af3b68e01394e95
HuynhDuc <huynhduc96@gmail.com>
2020-06-04 09:29:33 +0700

    init project

---
+++ ProjectBasic/app/src/main/java/com/example/moviedb/screen/MainApplication.kt
@@ -0,0 +1,16 @@
+package com.example.moviedb.screen
+
+import android.app.Application
+import com.example.moviedb.di.appModules
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class MainApplication : Application(){
+  override fun onCreate() {
+    super.onCreate()
+    startKoin {
+      androidContext(this@MainApplication)
+      modules(appModules)
+    }
+  }
+}
