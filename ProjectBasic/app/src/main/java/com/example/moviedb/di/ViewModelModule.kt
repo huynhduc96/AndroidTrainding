351651fd89494c17812edc3396e4497bf5e9c982
HuynhDuc <huynhduc96@gmail.com>
2020-06-17 12:28:42 +0700

    refactor struct

--- ProjectBasic/app/src/main/java/com/example/moviedb/di/ViewModelModule.kt
+++ ProjectBasic/app/src/main/java/com/example/moviedb/di/ViewModelModule.kt
@@ -3,7 +3,6 @@ package com.example.moviedb.di
 import com.example.moviedb.screen.detail.DetailViewModel
 import com.example.moviedb.screen.favorite.FavoriteViewModel
 import com.example.moviedb.screen.home.HomeViewModel
-import com.example.moviedb.screen.search.SearchViewModel
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -11,5 +10,4 @@ val viewModelModule = module {
     viewModel { HomeViewModel(get()) }
     viewModel { FavoriteViewModel(get()) }
     viewModel { DetailViewModel(get()) }
-    viewModel { SearchViewModel() }
 }
--- ProjectBasic/app/src/main/java/com/example/moviedb/screen/search/SearchViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.example.moviedb.screen.search
-
-import com.example.moviedb.screen.base.BaseViewModel
-
-class SearchViewModel : BaseViewModel() {
-}
